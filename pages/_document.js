dfc32c307af8bb85a5c43d80e6e31373831932eb
Mufli Fadla <muflifadlayt38@gmail.com>
2022-09-26 01:57:29 +0800

    configure web layout

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Documents = () => {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Documents;
