6e0a60d4c675df6681abc47ce51096c7028e5cdf
Mufli Fadla <muflifadlayt38@gmail.com>
2022-09-26 01:57:51 +0800

    clear unused element

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Geoenvi</title>
-        <meta name="description" content="Geo Environments Company" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <p>Hello World!</p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a href="/" target="_blank" rel="noopener noreferrer">
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/logo.png" alt="Vercel Logo" width={67} height={30} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <>
+      <p>Ini halaman Home.</p>
+    </>
   );
-}
+};
+
+export default Home;
