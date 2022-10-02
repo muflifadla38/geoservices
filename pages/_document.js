861471ccffa141b801641fd31204eab94e580551
Mufli Fadla <muflifadlayt38@gmail.com>
2022-10-02 22:07:57 +0800

    add scroll-smooth

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 const Documents = () => {
   return (
-    <Html>
+    <Html className="scroll-smooth">
       <Head>
         <link rel="icon" href="/logo-3.ico" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
