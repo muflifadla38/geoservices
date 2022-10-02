6ec53d46e32804d5bc8d9e19ce3400d24107c6f7
Mufli Fadla <muflifadlayt38@gmail.com>
2022-10-02 15:26:51 +0800

    remove invert filter & change logo

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  // const [scroll, setScroll] = useState(false);
+  // const newClass = scroll
+  //   ? "lg:border-slate-900/10 dark:border-slate-50/[0.06] invert"
+  //   : "";
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", () => {
+  //     setScroll(window.scrollY > 50);
+  //   });
+  // }, [scroll]);
+
   return (
-    <header id="header" className="px-20">
+    // <header
+    //   id="header"
+    //   className={
+    //     "px-20 sticky top-0 z-40 w-full backdrop-blur flex-none  lg:z-50  bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent drop-shadow-lg " +
+    //     newClass
+    //   }
+    // >
+    <header
+      id="header"
+      className="px-20 sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white supports-backdrop-blur:bg-white/95"
+    >
       <nav className="text-center font-semibold">
-        <ul className="md:flex md:justify-between align-middle m-4">
+        <ul className="md:flex md:justify-between align-middle m-2 p-2">
           <li className="my-auto">
             <Link href="/about">
               <a className="hover:text-violet-600">About</a>
@@ -25,7 +47,7 @@ const Header = () => {
             <Link href="/">
               <a>
                 <Image
-                  src="/logo.png"
+                  src="/logo-3.png"
                   alt="Company Logo"
                   width={90}
                   height={40}
