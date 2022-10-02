90a1106affb241aa27ddf8481c3725cb50715542
Mufli Fadla <muflifadlayt38@gmail.com>
2022-10-02 18:38:15 +0800

    fix border on hover

--- components/Header.js
+++ components/Header.js
@@ -3,43 +3,39 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Header = () => {
-  // const [scroll, setScroll] = useState(false);
-  // const newClass = scroll
-  //   ? "lg:border-slate-900/10 dark:border-slate-50/[0.06] invert"
-  //   : "";
+  const [scroll, setScroll] = useState(false);
+  const newClass = scroll
+    ? "lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06]"
+    : "";
 
-  // useEffect(() => {
-  //   window.addEventListener("scroll", () => {
-  //     setScroll(window.scrollY > 50);
-  //   });
-  // }, [scroll]);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 50);
+    });
+  }, [scroll]);
 
   return (
-    // <header
-    //   id="header"
-    //   className={
-    //     "px-20 sticky top-0 z-40 w-full backdrop-blur flex-none  lg:z-50  bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent drop-shadow-lg " +
-    //     newClass
-    //   }
-    // >
     <header
       id="header"
-      className="px-20 sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white supports-backdrop-blur:bg-white/95"
+      className={
+        "px-20 sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 bg-white supports-backdrop-blur:bg-white/95 " +
+        newClass
+      }
     >
       <nav className="text-center font-semibold">
         <ul className="md:flex md:justify-between align-middle m-2 p-2">
           <li className="my-auto">
-            <Link href="/about">
+            <Link href="#about">
               <a className="hover:text-violet-600">About</a>
             </Link>
           </li>
           <li className="my-auto">
-            <Link href="/services">
+            <Link href="#services">
               <a className="hover:text-violet-600">Services</a>
             </Link>
           </li>
           <li className="my-auto">
-            <Link href="/products">
+            <Link href="/#products">
               <a className="hover:text-violet-600">Products</a>
             </Link>
           </li>
@@ -56,12 +52,12 @@ const Header = () => {
             </Link>
           </li>
           <li className="my-auto">
-            <Link href="/locations">
+            <Link href="/#locations">
               <a className="hover:text-violet-600">Locations</a>
             </Link>
           </li>
           <li className="my-auto">
-            <Link href="/contact">
+            <Link href="/#contact">
               <a className="hover:text-violet-600">Contact</a>
             </Link>
           </li>
