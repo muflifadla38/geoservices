aa7e9c69f9371c69e76ddba81faeec480cb9bebc
Mufli Fadla <muflifadlayt38@gmail.com>
2022-10-08 01:41:28 +0800

    add arrow icon

--- components/Icons.js
+++ components/Icons.js
@@ -1,3 +1,35 @@
+export const LeftArrowIcon = ({ ...rest }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      {...rest}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
+      />
+    </svg>
+  );
+};
+
+export const RightArrowIcon = ({ ...rest }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      {...rest}
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+    >
+      <path d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
+    </svg>
+  );
+};
+
 export const ChevronDownIcon = ({ ...rest }) => {
   return (
     <svg
