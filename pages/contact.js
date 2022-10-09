ffe1d302ddb959eddbb115ca047b1b5547bd68a6
Mufli Fadla <muflifadlayt38@gmail.com>
2022-10-09 23:45:42 +0800

    Add Contact pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,104 @@
+import Link from "next/link";
+
 const Contact = () => {
   return (
     <>
-      <p>Contact</p>
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <iframe
+              width="100%"
+              height="100%"
+              className="absolute inset-0"
+              frameborder="0"
+              title="map"
+              marginheight="0"
+              marginwidth="0"
+              scrolling="no"
+              loading="lazy"
+              src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=Geophysics%20Departemen%20of%20Hasanuddin%20University&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+              style={{
+                filter: "grayscale(0.4)",
+                contrast: "1.2",
+                opacity: "0.5",
+              }}
+            ></iframe>
+            <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+              <div className="lg:w-1/2 px-6">
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  ADDRESS
+                </h2>
+                <p className="mt-1">
+                  Jl. Perintis Kemerdekaan KM. 10, Kel. Tamalanrea Indah, Kec.
+                  Tamalanrea, Kota Makassar, Sulawesi Selatan 90245
+                </p>
+              </div>
+              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <Link href="mailto:muflyfadlasyihab@gmail.com">
+                  <a className="text-indigo-500 leading-relaxed">
+                    muflyfadlasyihab@gmail.com
+                  </a>
+                </Link>
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
+                <Link href="tel:+6289669644474">
+                  <a className="leading-relaxed">0896-6964-4474</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <h2 className="text-gray-900 text-lg mb-1 font-semibold title-font">
+              Get in Touch With Us
+            </h2>
+            <p className="leading-relaxed mb-5 text-gray-600">
+              Need help with our service plans? Here's how you can reach us
+            </p>
+            <div className="relative mb-4">
+              <label for="name" className="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label for="email" className="leading-7 text-sm text-gray-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label for="message" className="leading-7 text-sm text-gray-600">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
+            <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Send
+            </button>
+            <p className="text-xs text-gray-500 mt-3">
+              Entah apa disini, bisa jga dihilangkan klo nda perlu
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
