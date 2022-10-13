7a9daf1e3bdae6c15ace1d057ed04e3f7fa53f48
Mufli Fadla <muflifadlayt38@gmail.com>
2022-10-13 08:58:04 +0800

    fix z-index

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -29,7 +29,7 @@ const Dropdown = ({ title, items }) => {
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-            <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0">
+            <Popover.Panel className="absolute left-1/2 z-50 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0">
               <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                 <div className="relative grid gap-6 bg-slate-50 dark:bg-slate-800 px-5 py-6 sm:gap-8 sm:p-8">
                   {items.map((item) => (
