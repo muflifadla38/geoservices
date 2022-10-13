46f07f3b910c9b8737607c8f82cba1912d93db56
Mufli Fadla <muflifadlayt38@gmail.com>
2022-10-13 08:39:35 +0800

    add darmode filled & flag icon

--- components/Icons.js
+++ components/Icons.js
@@ -723,6 +723,65 @@ export const NotFoundIcon = ({ ...rest }) => {
   );
 };
 
+export const IDIcon = ({ ...rest }) => {
+  return (
+    <svg
+      {...rest}
+      xmlns="http://www.w3.org/2000/svg"
+      width="1200"
+      height="800"
+      viewBox="0 0 3 2"
+    >
+      <path fill="#ce1126" d="M0 0H3V2H0z"></path>
+      <path fill="#fff" d="M0 1H3V2H0z"></path>
+    </svg>
+  );
+};
+
+export const USIcon = ({ ...rest }) => {
+  return (
+    <svg
+      {...rest}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="1235"
+      height="650"
+      viewBox="0 0 7410 3900"
+    >
+      <path fill="#b22234" d="M0 0H7410V3900H0z"></path>
+      <path
+        stroke="#fff"
+        strokeWidth="300"
+        d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
+      ></path>
+      <path fill="#3c3b6e" d="M0 0H2964V2100H0z"></path>
+      <g fill="#fff">
+        <g id="s18">
+          <g id="s9">
+            <g id="s5">
+              <g id="s4">
+                <path
+                  id="s"
+                  d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"
+                ></path>
+                <use y="420" xlinkHref="#s"></use>
+                <use y="840" xlinkHref="#s"></use>
+                <use y="1260" xlinkHref="#s"></use>
+              </g>
+              <use y="1680" xlinkHref="#s"></use>
+            </g>
+            <use x="247" y="210" xlinkHref="#s4"></use>
+          </g>
+          <use x="494" xlinkHref="#s9"></use>
+        </g>
+        <use x="988" xlinkHref="#s18"></use>
+        <use x="1976" xlinkHref="#s9"></use>
+        <use x="2470" xlinkHref="#s5"></use>
+      </g>
+    </svg>
+  );
+};
+
 export const TestIcon1 = ({ ...rest }) => {
   return (
     <svg
