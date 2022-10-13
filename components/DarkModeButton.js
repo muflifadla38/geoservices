5e70ef096d76fabda58ce514e7ff815d82f5342c
Mufli Fadla <muflifadlayt38@gmail.com>
2022-10-13 08:45:13 +0800

    fix color icon

--- components/DarkModeButton.js
+++ components/DarkModeButton.js
@@ -6,7 +6,7 @@ const DarkModeButton = () => {
   return (
     <>
       <button
-        className="bg-slate-200 dark:bg-slate-700 text-gray-500 dark:text-slate-400 hover:bg-slate-300 dark:hover:bg-slate-600 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 transition-all duration-300 ease-in-out"
+        className="bg-slate-200 dark:bg-slate-700 text-slate-600 dark:text-slate-400 hover:bg-slate-300 dark:hover:bg-slate-600 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 transition-all duration-300 ease-in-out"
         onClick={() => {
           setTheme(theme === "dark" ? "light" : "dark");
         }}
