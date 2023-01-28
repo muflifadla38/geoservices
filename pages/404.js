60f578f2bc6f23404e160dc25c0090591f5d5ca4
Mufli Fadla <muflifadlayt38@gmail.com>
2023-01-28 14:54:00 +0800

    add darkmode class

--- pages/404.js
+++ pages/404.js
@@ -4,12 +4,12 @@ import { NotFoundIcon, LeftArrowIcon } from "../components/Icons";
 const NotFound404 = () => {
   return (
     <div>
-      <div className="min-w-screen min-h-screen bg-blue-100 flex items-center p-5 lg:p-20 overflow-hidden relative rounded-3xl">
-        <div className="flex-1 min-h-full min-w-full rounded-3xl bg-white shadow-xl p-10 lg:p-20 text-gray-800 relative md:flex items-center text-center md:text-left">
+      <div className="min-w-screen min-h-screen bg-blue-100 dark:bg-slate-700 flex items-center p-5 lg:p-20 overflow-hidden relative rounded-3xl">
+        <div className="flex-1 min-h-full min-w-full rounded-3xl bg-white dark:bg-slate-800 shadow-xl p-10 lg:p-20 text-gray-800 relative md:flex items-center text-center md:text-left">
           <div className="w-full md:w-1/2">
             <div className="mb-10 lg:mb-20"></div>
-            <div className="mb-10 md:mb-20 text-gray-600 font-light">
-              <h1 className="font-black uppercase text-3xl lg:text-5xl text-yellow-500 mb-10">
+            <div className="mb-10 md:mb-20 text-slate-500 dark:text-slate-400 font-light">
+              <h1 className="font-black uppercase text-3xl lg:text-5xl text-slate-500 mb-10">
                 You seem to be lost!
               </h1>
               <p>The page you&apos;re looking for isn&apos;t available.</p>
@@ -17,7 +17,7 @@ const NotFound404 = () => {
             </div>
             <div className="mb-20 md:mb-0">
               <Link href="/">
-                <button className="text-lg font-light outline-none focus:outline-none transform transition-all hover:scale-110 text-yellow-500 hover:text-yellow-600 inline-flex">
+                <button className="text-lg font-light outline-none focus:outline-none transform transition-all hover:scale-110 text-slate-500 hover:text-sky-600 inline-flex">
                   <LeftArrowIcon className="mr-2 w-5 h-6" />
                   <span>Go Back</span>
                 </button>
@@ -28,8 +28,8 @@ const NotFound404 = () => {
             <NotFoundIcon />
           </div>
         </div>
-        <div className="w-64 md:w-96 h-96 md:h-full bg-blue-200 bg-opacity-30 absolute -top-64 md:-top-96 right-20 md:right-32 rounded-full pointer-events-none -rotate-45 transform"></div>
-        <div className="w-96 h-full bg-yellow-200 bg-opacity-20 absolute -bottom-96 right-64 rounded-full pointer-events-none -rotate-45 transform"></div>
+        <div className="w-64 md:w-96 h-96 md:h-full bg-sky-200 dark:bg-sky-800 bg-opacity-30 dark:bg-opacity-30 absolute -top-64 md:-top-96 right-20 md:right-32 rounded-full pointer-events-none -rotate-45 transform"></div>
+        <div className="w-96 h-full bg-yellow-200 dark:bg-slate-400 bg-opacity-20 dark:bg-opacity-20 absolute -bottom-96 right-64 rounded-full pointer-events-none -rotate-45 transform"></div>
       </div>
     </div>
   );
